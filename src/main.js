57d9c3337e6999b81500a1f289f3d409850405b9
Boogle <theboogledoo@gmail.com>
2025-02-17 15:49:03 -0600

    setup

--- src/main.js
+++ src/main.js
@@ -1,17 +1,1 @@
 "use strict";
-
-var settings = require('./settings.js');
-var launcher = require('./launcher.js');
-var server = require('./server.js');
-
-var emulationSettings = settings.getEmulationSettings();
-
-// launcher logic
-if (emulationSettings.emulateLauncher) {
-	launcher.start();
-}
-
-// server logic
-if (emulationSettings.emulateServer) {
-	server.start();
-}
